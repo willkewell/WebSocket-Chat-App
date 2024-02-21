55db74fcaed697a337260de47e992031319d2998
willkewell <william.kewell2@gmail.com>
2024-02-21 17:44:23 +0000

    Initial

---
+++ ws-chat-app/src/main/java/com/websocket/wschatapp/WsChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.websocket.wschatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsChatAppApplication.class, args);
+	}
+
+}
